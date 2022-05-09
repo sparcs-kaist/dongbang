7c32ced6378b6f4f39cebb7037f526770a3944ef
Soonho Kwon <percy3368@gmail.com>
2022-05-09 10:58:37 +0900

    feat: Extend user collection schema

--- imports/db/users.ts
+++ imports/db/users.ts
@@ -2,22 +2,35 @@ import {Meteor} from "meteor/meteor";
 import SimpleSchema from "simpl-schema";
 
 
-const Options = (...values: string[]) =>
-    new RegExp(`^${values.map(v => `(${v})`).join("|")}$`);
+// const Options = (...values: string[]) =>
+//     new RegExp(`^${values.map(v => `(${v})`).join("|")}$`);
+
+const Enum = (enumObj: Object) =>
+    new RegExp(`^${
+        Object
+            .values(enumObj)
+            .map(v => `(${v})`)
+            .join("|")
+    }$`);
 
 
 export enum UserStatus {
-
+    PRESENT = "userstatus:present",
+    SESSION = "userstatus:session",
+    OFFLINE = "userstatus:offline",
 }
 
+
 export interface ExtendUser {
+    name: string;
     nickname: string;
-    
+    status: UserStatus;
 }
 
 const userSchema = new SimpleSchema({
-    username: String,
-    qwer: Options("aaa", "asdf", "sdddf"),
+    name: String,
+    nickname: String,
+    status: Enum(UserStatus)
 })
 
 Meteor.users.attachSchema(userSchema);

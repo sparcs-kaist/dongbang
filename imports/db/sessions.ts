ee7793b3789e784e9ec2197d7cba54c1e965e97f
Soonho Kwon <percy3368@gmail.com>
2022-05-11 16:56:04 +0900

    feat: Initialize session collection structure

--- imports/db/sessions.ts
+++ imports/db/sessions.ts
@@ -1,2 +1,20 @@
 import {Mongo} from "meteor/mongo";
+import SimpleSchema from "simpl-schema";
 
+export enum Location {
+    A_SITE = "location:a_site",
+    B_SITE = "location:b_site",
+}
+
+export interface Session {
+    name: string,
+    location?: Location,
+}
+
+export const SessionCollection = new Mongo.Collection<Session>("sessions");
+
+SessionCollection.schema = new SimpleSchema({
+
+})
+
+SessionCollection.attachSchema(SessionCollection.schema);

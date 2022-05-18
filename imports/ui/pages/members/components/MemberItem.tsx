d76f1d2ee383c297d3e53ab68caaf0423cccf36a
Soonho Kwon <percy3368@gmail.com>
2022-05-18 19:45:43 +0900

    feat: Add memberItem design

--- imports/ui/pages/members/components/MemberItem.tsx
+++ imports/ui/pages/members/components/MemberItem.tsx
@@ -1,6 +1,6 @@
 import {Meteor} from "meteor/meteor";
 
-import React from "react";
+import React, {ReactNode} from "react";
 import styles from "./MemberItem.module.css";
 import {useNavigate} from "react-router-dom";
 
@@ -12,17 +12,23 @@ interface MemberItemProps {
 const MemberItem: React.FC<MemberItemProps> = ({member}) => {
     const navigate = useNavigate();
     
+    const userStatus: ReactNode = member.session
+        ? <span>{member.session.name} 참여 중</span>
+        : <span>{member.status?.message || "동방"}</span>
+    
     return (
         <div
             className={styles.item}
             onClick={() => navigate(`${member.username}`, {replace: true})}
         >
             <div className={styles.inner}>
-                <p>
-                    {member.name}
+                <p className={styles.userMain}>
+                    <span>{member.name}</span>
+                    <span>{member.username}</span>
                 </p>
-                <p>
-                
+                <p className={styles.userDesc}>
+                    <div className={styles.indicator}/>
+                    {userStatus}
                 </p>
             </div>
         </div>

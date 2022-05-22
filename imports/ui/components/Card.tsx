eeaf0f66959ee4d64fba0769fc10020cc35bdfc2
Soonho Kwon <percy3368@gmail.com>
2022-05-22 20:27:42 +0900

    feat: Extend Card component props

--- imports/ui/components/Card.tsx
+++ imports/ui/components/Card.tsx
@@ -1,18 +1,15 @@
-import React from "react";
+import React, {HTMLAttributes} from "react";
 import styles from "./Card.module.css";
 import classNames from "classnames";
 
-import {CommonProps} from "./commonProps";
-
-interface PaperProps extends CommonProps {
+interface PaperProps extends HTMLAttributes<HTMLDivElement> {
     clickable?: boolean;
-    onClick?: () => void;
 }
 
 export const Card: React.FC<PaperProps> = (
-    {children, className}
+    {children, className, ...props}
 ) => (
-    <div className={classNames(styles.card, className)}>
+    <div className={classNames(styles.card, className)} {...props}>
         {children}
     </div>
 );

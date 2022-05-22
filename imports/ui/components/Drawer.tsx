1e468d1cdf00ac08588a4099df6ca2a63783e4d8
Soonho Kwon <percy3368@gmail.com>
2022-05-23 02:22:54 +0900

    feat: Add animations

--- imports/ui/components/Drawer.tsx
+++ imports/ui/components/Drawer.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from "react";
+import React, {ReactNode, useState} from "react";
 import styles from "./Drawer.module.css";
 
 import {useNavigate} from "react-router-dom";
@@ -10,15 +10,30 @@ interface DrawerProps {
 const Drawer: React.FC<DrawerProps> = ({children}) => {
     const navigate = useNavigate();
     
-    return (<>
-        <div className={styles.drawer}>
+    type State = typeof styles.open | typeof styles.close | null;
+    const [animationState, setAnimationState] = useState<State>(styles.open);
+    
+    
+    const onAnimationEnd = () => {
+        switch (animationState) {
+            case styles.open:
+                setAnimationState(null);
+                break;
+            case styles.close:
+                navigate("..", {replace: true})
+                break;
+        }
+    }
+    
+    return (<div className={animationState}>
+        <div className={styles.drawer} onAnimationEnd={onAnimationEnd}>
             {children}
         </div>
         <div
             className={styles.backdrop}
-            onClick={() => navigate("..", {replace: true})}
+            onClick={() => setAnimationState(styles.close)}
         />
-    </>)
+    </div>)
 }
 
 export default Drawer;

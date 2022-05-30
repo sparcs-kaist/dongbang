40ef04c1711e47fa5304056db7aca9698a248211
Soonho Kwon <percy3368@gmail.com>
2022-05-30 20:31:51 +0900

    feat: Add toggle status feature

--- app/imports/ui/pages/members/Member.tsx
+++ app/imports/ui/pages/members/Member.tsx
@@ -3,10 +3,13 @@ import {Meteor} from "meteor/meteor";
 import React from "react";
 import styles from "./Member.module.css";
 
+import {useParams} from "react-router-dom";
+
 import Drawer from "/imports/ui/components/Drawer";
 import {Text, renderProfileText} from "/imports/ui/components/Text";
+import {Button} from "/imports/ui/components/Button";
 
-import {useParams} from "react-router-dom";
+import {updateStatus} from "/imports/api/methods/members";
 
 
 interface MemberProps {
@@ -18,15 +21,27 @@ const Member: React.FC<MemberProps> = ({user, members}) => {
     const {username} = useParams();
     const isSelf = user?.username === username;
     
+    const toggleStatus = () => {
+        updateStatus.call({
+            isActive: !user?.isActive,
+        });
+    }
+    
     
     const member = isSelf
         ? user
         : members?.find(member => member.username === username);
     
     return (
-        <Drawer>
+        <Drawer className={styles.root}>
             {member ? <>
                 <Text.main>{renderProfileText(member)}</Text.main>
+    
+    
+                {isSelf && <div className={styles.buttonContainer}>
+                    <Button onClick={toggleStatus}>상태 변경</Button>
+                </div>}
+                
             </> : <>
             
             

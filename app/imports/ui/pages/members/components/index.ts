05193389b0a1d93991f8f00f7b268434344bef41
Soonho Kwon <percy3368@gmail.com>
2022-07-01 15:37:59 +0900

    feat: Generate UI component for device registeration

--- app/imports/ui/pages/members/components/index.ts
+++ app/imports/ui/pages/members/components/index.ts
@@ -1,9 +1,11 @@
 import MemberItem from "./MemberItem";
 import MemberContainer from "./MemberContainer";
 import RequireLogin from "./RequireLogin";
+import DeviceRegister from "./DeviceRegister";
 
 export {
     MemberItem,
     MemberContainer,
     RequireLogin,
+    DeviceRegister,
 }

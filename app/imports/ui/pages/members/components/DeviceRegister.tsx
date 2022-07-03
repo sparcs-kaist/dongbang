01312fbd2b9a47791521d312e1aff235cb12af74
Soonho Kwon <percy3368@gmail.com>
2022-07-03 14:09:16 +0900

    feat: Device register check feature

--- app/imports/ui/pages/members/components/DeviceRegister.tsx
+++ app/imports/ui/pages/members/components/DeviceRegister.tsx
@@ -2,23 +2,36 @@ import React, {useEffect, useState} from "react";
 import {Card, CardBody, CardText} from "/imports/ui/components/Card";
 import {AnimatePresence} from "framer-motion";
 import {Button} from "/imports/ui/components/Button";
+import {Spacer} from "/imports/ui/components/Spacer";
+
 
 const DeviceRegister: React.FC = () => {
-    const {active} = useDeviceRegister();
-    
+    const {active, register, error} = useDeviceRegister();
     
     return (
         <AnimatePresence>
             {active && <Card
-                initial={{opacity: 0, height: 0}}
-                animate={{opacity: 1, height: "auto"}}
-                exit={{opacity: 0, height: 0}}
+                initial={{opacity: 0, height: 0, marginBottom: 0}}
+                animate={{opacity: 1, height: "auto", marginBottom: 10}}
+                exit={{opacity: 0, height: 0, marginBottom: 0}}
             >
                 <CardBody>
-                    <CardText.main>기기 등록</CardText.main>
-                    <CardText.sub>
-                        아래의 기기를 등록하시면, 다른 사용자들이 당신을 찾을 수 있습니다.</CardText.sub>
-                    <Button>asdfasdf</Button>
+                    {error ? <>
+                        <CardText.main>에러 발생</CardText.main>
+                        <CardText.sub>
+                            이 메시지가 보이면 SPARCS AP presence check server가 정상적으로 동작하지 않고 있다는 의미입니다.
+                            관리자에게 연락해 주세요.
+                            <Spacer y={5}/>
+                            detail: {error}
+                        </CardText.sub>
+                    </> : <>
+                        <CardText.main> 내 기기 등록하기</CardText.main>
+                        <CardText.sub>
+                            SPARCS AP 연결 상태에 따라 내 상태가 변경됩니다
+                        </CardText.sub>
+                        <Spacer y={10}/>
+                        <Button onClick={register}>기기 등록하기</Button>
+                    </>}
                 </CardBody>
             </Card>}
         </AnimatePresence>
@@ -26,24 +39,54 @@ const DeviceRegister: React.FC = () => {
 }
 
 const useDeviceRegister = () => {
-    const [active, setActive] = useState(false);
+    const [macAddress, setMacAddress] = useState<string | undefined>();
+    const [error, setError] = useState<string | undefined>();
     
     useEffect(() => {
-        checkRegistration().then(() => {
-            setActive(true);
-        });
-    }, []);
+        fetchData()
+            .then(({macAddress, error}) => {
+                setMacAddress(macAddress);
+                setError(error);
+            })
+            .catch();
+    }, [setMacAddress, setError]);
     
-    return {active};
+    
+    const register = () => {
+        if (macAddress) {
+            // Do something
+            
+            setMacAddress(undefined);
+        }
+    }
+    
+    return {
+        active: !!macAddress || error,
+        register,
+        error,
+    };
 }
 
 
-const checkRegistration = async () => {
-    await fetch("http://192.168.0.74:57463/dongbang");
+interface Data {
+    macAddress?: string;
+    error?: string;
 }
 
-const register = async () => {
-
+const fetchData = async (): Promise<Data> => {
+    const res = await fetch("http://172.30.113.37:57463/dongbang");
+    
+    if (res.status !== 200) throw new Error(res.statusText);
+    
+    const data = await res.json();
+    console.log(data);
+    
+    if (data?.hello !== "dongbang") throw new Error("Invalid response");
+    
+    if (typeof data?.error === "string") return {error: data.error};
+    if (typeof data?.macAddress === "string") return {macAddress: data.macAddress};
+    
+    throw new Error("Invalid response");
 }
 
 export default DeviceRegister;

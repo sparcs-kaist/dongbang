31e5125bf7a3e13b54e02a0f311e392058115e42
Soonho Kwon <percy3368@gmail.com>
2022-08-16 03:04:34 +0900

    fix: Fix and optimize cleanup obeserver logic

--- app/imports/observers/users.ts
+++ app/imports/observers/users.ts
@@ -2,18 +2,21 @@ import { collections } from "../collections";
 
 collections.users.find({}, { fields: { sessionId: 1 } }).observe({
     changed(curr, prev) {
-        curr.sessionId
-            ? // Update `isActive` to true when user joins a session
-              !prev.sessionId &&
-              collections.users.update(curr, {
-                  $set: { isActive: true },
-              })
-            : // Cleanup an empty session
-              prev.sessionId &&
-              collections.sessions
-                  .getLink(prev.sessionId, "members")
-                  .find()
-                  .count() === 0 &&
-              collections.sessions.remove(prev.sessionId);
+        if (curr.sessionId !== prev.sessionId) {
+            // Update `isActive` to true when user joins a session
+            curr.sessionId &&
+                !prev.sessionId &&
+                collections.users.update(curr, {
+                    $set: { isActive: true },
+                });
+
+            // Cleanup an empty session
+            prev.sessionId &&
+                collections.sessions
+                    .getLink(prev.sessionId, "members")
+                    .find()
+                    .count() === 0 &&
+                collections.sessions.remove(prev.sessionId);
+        }
     },
 });

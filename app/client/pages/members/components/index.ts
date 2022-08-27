aa1ae77e2374c99e3d3c70370a99476697e403d8
Soonho Kwon <percy3368@gmail.com>
2022-08-28 05:44:12 +0900

    chore: Remove unused component

--- app/client/pages/members/components/index.ts
+++ app/client/pages/members/components/index.ts
@@ -1,11 +1,3 @@
-// import MemberItem from "./MemberItem";
-// import MemberContainer from "./MemberContainer";
-// import RequireLogin from "./RequireLogin";
-// import DeviceRegister from "./DeviceRegister";
-//
-// export { MemberItem, MemberContainer, RequireLogin, DeviceRegister };
-
-export * from "./DeviceRegister";
 export * from "./MemberContainer";
 export * from "./MemberItem";
 export * from "./MemberStatus";
